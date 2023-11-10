26d92f85cff2377f9f51b8174efcc9f2eefa2b86
cicada7772 <accountsemail7772@protonmail.com>
2023-11-09 22:03:59 -0500

    transferred parts of RobotAutoDriveToAprilTagTank.java into AutoMain.java this includes finding april tags in a list and identifying a desired tag. the robot will then move towards it (currently). TODO: set desired april tag and tell robot to move to the tape on the ground using its position information from the april tag.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/op/auto/AutoMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/op/auto/AutoMain.java
@@ -31,11 +31,14 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraName;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
@@ -43,6 +46,7 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /*
  * This OpMode illustrates the concept of driving a path based on encoder counts.
@@ -75,32 +79,50 @@ import java.util.List;
 public class AutoMain extends LinearOpMode {
 
     /* Declare OpMode members. */
-    private DcMotor left1   = null;
-    private DcMotor left2   = null;
-    private DcMotor right1  = null;
-    private DcMotor right2  = null;
+    private DcMotor left1 = null;
+    private DcMotor left2 = null;
+    private DcMotor right1 = null;
+    private DcMotor right2 = null;
 
     //basic typical opmode stuff
-    private ElapsedTime     runtime = new ElapsedTime();
+    private ElapsedTime runtime = new ElapsedTime();
 
     //math to make convert motor rotations to wheel distance
-    static final double     COUNTS_PER_MOTOR_REV    = 537.6 ;    // neverest 20
-    static final double     DRIVE_GEAR_REDUCTION    = (15.0/20.0) ;     // No External Gearing.
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
-    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double COUNTS_PER_MOTOR_REV = 537.6;    // neverest 20
+    static final double DRIVE_GEAR_REDUCTION = (15.0 / 20.0);     // No External Gearing.
+    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
     //speeds
-    static final double     DRIVE_SPEED             = 0.1;
-    static final double     TURN_SPEED              = 0.5;
+    static final double DRIVE_SPEED = 0.1;
+    static final double TURN_SPEED = 0.5;
+    final double SPEED_GAIN =   0.02 ;   //  Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
+    final double TURN_GAIN  =   0.01 ;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
 
     //super intelligent vision stuff
+    private static final int DESIRED_TAG_ID = -1;
+    private AprilTagDetection desiredTag = null;
     private WebcamName webcam1, webcam2;
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
     private AprilTagProcessor aprilTag;
     private TfodProcessor tfod;
     private VisionPortal visionPortal;
 
+    /*
+     *  Method to perform a relative move, based on encoder counts.
+     *  Encoders are not reset as the move is based on the current position.
+     *  Move will stop if any of three conditions occur:
+     *  1) Move gets to the desired position
+     *  2) Move runs out of time
+     *  3) Driver stops the OpMode running.
+     */
     @Override
     public void runOpMode() {
+        boolean targetFound = false;    // Set to true when an AprilTag target is detected
+        double drive = 0;        // Desired forward power/speed (-1 to +1) +ve is forward
+        double turn = 0;        // Desired turning power/speed (-1 to +1) +ve is CounterClockwise
+
+        // Initialize the Apriltag Detection process
+        initDoubleVision();
 
         // Initialize the drive system variables.
         left1 = hardwareMap.get(DcMotor.class, "L1");
@@ -126,8 +148,10 @@ public class AutoMain extends LinearOpMode {
         right1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         right2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        setManualExposure(6, 250);  // Use low exposure time to reduce motion blur
+
         // Send telemetry message to indicate successful Encoder reset
-        telemetry.addData("Starting at",  "%7d :%7d",
+        telemetry.addData("Starting at", "%7d :%7d",
                 left1.getCurrentPosition(),
                 left2.getCurrentPosition(),
                 right1.getCurrentPosition(),
@@ -144,19 +168,53 @@ public class AutoMain extends LinearOpMode {
         visionPortal.setProcessorEnabled(tfod, false);
         visionPortal.setProcessorEnabled(aprilTag, true);
 
-        telemetry.addData("Path", "Complete");
-        telemetry.update();
-        sleep(1000);  // pause to display final telemetry message.
-    }
+        while (opModeIsActive()) {
+            targetFound = false;
+            desiredTag = null;
 
-    /*
-     *  Method to perform a relative move, based on encoder counts.
-     *  Encoders are not reset as the move is based on the current position.
-     *  Move will stop if any of three conditions occur:
-     *  1) Move gets to the desired position
-     *  2) Move runs out of time
-     *  3) Driver stops the OpMode running.
-     */
+            // Step through the list of detected tags and look for a matching tag
+            List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+            for (AprilTagDetection detection : currentDetections) {
+                // Look to see if we have size info on this tag.
+                if (detection.metadata != null) {
+                    //  Check to see if we want to track towards this tag.
+                    if ((DESIRED_TAG_ID < 0) || (detection.id == DESIRED_TAG_ID)) {
+                        // Yes, we want to use this tag.
+                        targetFound = true;
+                        desiredTag = detection;
+                        break;  // don't look any further.
+                    } else {
+                        // This tag is in the library, but we do not want to track it right now.
+                        telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
+                    }
+                } else {
+                    // This tag is NOT in the library, so we don't have enough information to track to it.
+                    telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
+                }
+                if (gamepad1.left_bumper && targetFound) {
+
+                    // Determine heading and range error so we can use them to control the robot automatically.
+                    double  rangeError   = (desiredTag.ftcPose.range - 12); //second number is desired distance from tag
+                    double  headingError = desiredTag.ftcPose.bearing;
+
+                    // Use the speed and turn "gains" to calculate how we want the robot to move.  Clip it to the maximum
+                    drive = Range.clip(rangeError * SPEED_GAIN, -DRIVE_SPEED, DRIVE_SPEED);
+                    turn  = Range.clip(headingError * TURN_GAIN, -TURN_SPEED, TURN_SPEED) ;
+
+                    telemetry.addData("Auto","Drive %5.2f, Turn %5.2f", drive, turn);
+                }
+                telemetry.update();
+
+                // Apply desired axes motions to the drivetrain.
+                moveRobot(drive, turn);
+                sleep(10);
+            }
+
+            telemetry.addData("Path", "Complete");
+            telemetry.update();
+            sleep(1000);  // pause to display final telemetry message.
+        }
+    }
     public void encoderDrive(double speed,
                              double leftInches, double rightInches,
                              double timeoutS) {
@@ -170,10 +228,10 @@ public class AutoMain extends LinearOpMode {
         if (opModeIsActive()) {
 
             // Determine new target position, and pass to motor controller
-            newLeft1Target = left1.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
-            newLeft2Target = left2.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
-            newRight1Target = right1.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
-            newRight2Target = right2.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            newLeft1Target = left1.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newLeft2Target = left2.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newRight1Target = right1.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            newRight2Target = right2.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
             left1.setTargetPosition(newLeft1Target);
             left2.setTargetPosition(newLeft2Target);
             right1.setTargetPosition(newRight1Target);
@@ -203,8 +261,8 @@ public class AutoMain extends LinearOpMode {
                     (left1.isBusy() && right1.isBusy())) {
 
                 // Display it for the driver.
-                telemetry.addData("Running to",  " %7d :%7d", newLeft1Target,  newRight1Target);
-                telemetry.addData("Currently at",  " at %7d :%7d",
+                telemetry.addData("Running to", " %7d :%7d", newLeft1Target, newRight1Target);
+                telemetry.addData("Currently at", " at %7d :%7d",
                         left1.getCurrentPosition(), right1.getCurrentPosition());
                 telemetry.update();
             }
@@ -225,24 +283,28 @@ public class AutoMain extends LinearOpMode {
         }
     }
 
-    private void initDoubleVision() {
+    public void initDoubleVision() {
         // -----------------------------------------------------------------------------------------
         // AprilTag Configuration
         // -----------------------------------------------------------------------------------------
 
         aprilTag = new AprilTagProcessor.Builder()
                 .build();
-
+        // Adjust Image Decimation to trade-off detection-range for detection-rate.
+        // eg: Some typical detection data using a Logitech C920 WebCam
+        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
+        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
+        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second
+        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second
+        // Note: Decimation can be changed on-the-fly to adapt during a match.
+        aprilTag.setDecimation(2);
         // -----------------------------------------------------------------------------------------
         // TFOD Configuration
-        // -----------------------------------------------------------------------------------------
 
         tfod = new TfodProcessor.Builder()
                 .build();
-
         // -----------------------------------------------------------------------------------------
         // Camera Configuration
-        // -----------------------------------------------------------------------------------------
 
         webcam1 = hardwareMap.get(WebcamName.class, "Webcam 1");
         webcam2 = hardwareMap.get(WebcamName.class, "Webcam 2");
@@ -254,12 +316,64 @@ public class AutoMain extends LinearOpMode {
                 .setCamera(switchableCamera)
                 .addProcessors(tfod, aprilTag)
                 .build();
-        }// end initDoubleVision()
+    }// end initDoubleVision()
+
+    private void setManualExposure(int exposureMS, int gain) {
+        // Wait for the camera to be open, then use the controls
+
+        if (visionPortal == null) {
+            return;
+        }
+
+        // Make sure camera is streaming before we try to set the exposure controls
+        if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
+            telemetry.addData("Camera", "Waiting");
+            telemetry.update();
+            while (!isStopRequested() && (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
+                sleep(20);
+            }
+            telemetry.addData("Camera", "Ready");
+            telemetry.update();
+        }
+
+        // Set camera controls unless we are stopping.
+        if (!isStopRequested()) {
+            ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
+            if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
+                exposureControl.setMode(ExposureControl.Mode.Manual);
+                sleep(50);
+            }
+            exposureControl.setExposure((long) exposureMS, TimeUnit.MILLISECONDS);
+            sleep(20);
+            GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
+            gainControl.setGain(gain);
+            sleep(20);
+            telemetry.addData("Camera", "Ready");
+            telemetry.update();
+        }
+    }
+    public void moveRobot(double x, double yaw) {
+        // Calculate left and right wheel powers.
+        double leftPower    = x - yaw;
+        double rightPower   = x + yaw;
+
+        // Normalize wheel powers to be less than 1.0
+        double max = Math.max(Math.abs(leftPower), Math.abs(rightPower));
+        if (max >1.0) {
+            leftPower /= max;
+            rightPower /= max;
+        }
+
+        // Send powers to the wheels.
+        left1.setPower(leftPower);
+        left2.setPower(leftPower);
+        right1.setPower(rightPower);
+        right2.setPower(rightPower);
+    }
 
     private void telemetryAprilTag() {
         List<AprilTagDetection> currentDetections = aprilTag.getDetections();
         telemetry.addData("# AprilTags Detected", currentDetections.size());
-
         // Step through the list of detections and display info for each one.
         for (AprilTagDetection detection : currentDetections) {
             if (detection.metadata != null) {
@@ -274,16 +388,17 @@ public class AutoMain extends LinearOpMode {
         }   // end for() loop
 
     }   // end method telemetryAprilTag()
+
     private void telemetryTfod() {
         List<Recognition> currentRecognitions = tfod.getRecognitions();
         telemetry.addData("# Objects Detected", currentRecognitions.size());
 
         // Step through the list of recognitions and display info for each one.
         for (Recognition recognition : currentRecognitions) {
-            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
-            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
+            double x = (recognition.getLeft() + recognition.getRight()) / 2;
+            double y = (recognition.getTop() + recognition.getBottom()) / 2;
 
-            telemetry.addData(""," ");
+            telemetry.addData("", " ");
             telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
             telemetry.addData("- Position", "%.0f / %.0f", x, y);
             telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
@@ -291,6 +406,5 @@ public class AutoMain extends LinearOpMode {
 
     }   // end method telemetryTfod()
 }
-
 
 
